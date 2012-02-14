878f56e509d525174aca333db38632f970561ea8
pablodip <pablodip@gmail.com>
2012-02-14 10:17:44 +0000

    changed http by git in vendors

--- vendors.php
+++ vendors.php
@@ -7,20 +7,20 @@ if (!is_dir($vendorDir = __DIR__.'/vendor')) {
 }
 
 $deps = array(
-    array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('twig', 'http://github.com/fabpot/Twig', 'origin/master'),
-    array('molino', 'http://github.com/pablodip/molino', 'origin/master'),
-    array('mondator', 'http://github.com/mandango/mondator', 'origin/master'),
-    array('mandango', 'http://github.com/mandango/mandango', 'origin/master'),
-    array('doctrine-common', 'http://github.com/doctrine/common', 'origin/master'),
-    array('doctrine-dbal', 'http://github.com/doctrine/dbal', 'origin/master'),
-    array('doctrine-orm', 'http://github.com/doctrine/doctrine2', 'origin/master'),
-    array('doctrine-mongodb', 'http://github.com/doctrine/mongodb', 'origin/master'),
-    array('doctrine-mongodb-odm', 'http://github.com/doctrine/mongodb-odm', 'origin/master'),
-    array('metadata', 'http://github.com/schmittjoh/metadata', 'origin/master'),
-    array('mandango-bundle', 'http://github.com/mandango/MandangoBundle', 'origin/master', 'bundles/Mandango/MandangoBundle'),
-    array('doctrine-bundle', 'http://github.com/doctrine/DoctrineBundle', 'origin/master', 'bundles/Doctrine/Bundle/DoctrineBundle'),
-    array('pablodip-module-bundle', 'http://github.com/pablodip/PablodipModuleBundle', 'origin/master', 'bundles/Pablodip/ModuleBundle'),
+    array('symfony', 'git://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('twig', 'git://github.com/fabpot/Twig', 'origin/master'),
+    array('molino', 'git://github.com/pablodip/molino', 'origin/master'),
+    array('mondator', 'git://github.com/mandango/mondator', 'origin/master'),
+    array('mandango', 'git://github.com/mandango/mandango', 'origin/master'),
+    array('doctrine-common', 'git://github.com/doctrine/common', 'origin/master'),
+    array('doctrine-dbal', 'git://github.com/doctrine/dbal', 'origin/master'),
+    array('doctrine-orm', 'git://github.com/doctrine/doctrine2', 'origin/master'),
+    array('doctrine-mongodb', 'git://github.com/doctrine/mongodb', 'origin/master'),
+    array('doctrine-mongodb-odm', 'git://github.com/doctrine/mongodb-odm', 'origin/master'),
+    array('metadata', 'git://github.com/schmittjoh/metadata', 'origin/master'),
+    array('mandango-bundle', 'git://github.com/mandango/MandangoBundle', 'origin/master', 'bundles/Mandango/MandangoBundle'),
+    array('doctrine-bundle', 'git://github.com/doctrine/DoctrineBundle', 'origin/master', 'bundles/Doctrine/Bundle/DoctrineBundle'),
+    array('pablodip-module-bundle', 'git://github.com/pablodip/PablodipModuleBundle', 'origin/master', 'bundles/Pablodip/ModuleBundle'),
 );
 
 foreach ($deps as $dep) {
@@ -42,7 +42,7 @@ foreach ($deps as $dep) {
         $install = true;
         echo "> Installing $name\n";
 
-        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+        system(sprintf('git clone -q %s %s', escapeshellarg($url), escapeshellarg($installDir)));
     }
 
     if (!$install) {
